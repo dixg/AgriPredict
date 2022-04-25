c47bca9c0c502acda715cf122a5121d8426400de
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-04-24 17:31:42 -0700

    chartjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import faker from "faker";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [1],
+    };
+  }
+  async componentDidMount() {
+    let response = await this.fetchData();
+    this.setState({ data: response });
+  }
+
+  fetchData = async () => {
+    let data = { commodity: "AMERICAN-COTTON", mandi_name: "ADAMPUR" };
+    let response = await axios.post(`http://127.0.0.1:8000/polls/`, data);
+    response = JSON.parse(JSON.stringify(response.data));
+    return response;
+  };
+
+  render() {
+    let x_axis = this.state.data.reduce((prev, current) => {
+      return [...prev, current.timeStr];
+    },[]);
+    let y_axis = this.state.data.reduce((prev, current) => {
+      return [...prev, current.value];
+    }, []);
+    console.log("Y-Axis", y_axis);
+    return (
+      <div>
+        <table>
+          <tr>
+            <th>Date</th>
+            <th>Value</th>
+          </tr>
+          {this.state.data.map((item) => (
+            <tr>
+              <td>{item.timeStr}</td>
+              <td>{item.value}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
